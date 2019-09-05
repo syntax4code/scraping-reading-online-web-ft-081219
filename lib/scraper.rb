86b24a7134a4e12264a3db1b62e412de9efc247f
Warren Harper II <warrenharper2@att.net>
2019-09-05 17:29:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,5 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
-Nokogiri::HTML(html)
-doc = Nokogiri::HTML(html)
-puts doc
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".site-header__hero__headline")
